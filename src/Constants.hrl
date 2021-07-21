ca80619c417d8ec06b4f4b3405ea24646f03f00d
Tomer Shaked <tomer1shaked@gmail.com>
2021-07-21 15:26:39 +0300

    added constants file for simulation

---
+++ src/Constants.hrl
@@ -0,0 +1,30 @@
+%%%-------------------------------------------------------------------
+%%% @author tomer
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 21. Jul 2021 12:52
+%%%-------------------------------------------------------------------
+-author("tomer").
+
+-define(WIN_WIDTH,600).
+-define(WIN_HEIGHT,800).
+-define(FLOOR,730).
+
+-define(MAX_ROTATION,25).
+-define(ROT_VEL,20).
+-define(ANIMATION_TIME,5).
+
+-define(PIPE_GAP,200).
+-define(X_VELOCITY,5).
+
+-define(BG_WIDTH,288).
+-define(BG_HIGHT,512).
+-define(BASE_WIDTH,336).
+-define(BASE_HIGHT,112).
+-define(PIPE_WIDTH,52).
+-define(PIPE_HIGHT,320).
+-define(BIRD_WIDTH,34).
+-define(BIRD_HIGHT,24).
+-define(BIRD_RADIUS,13).% averaged width and height to 26
